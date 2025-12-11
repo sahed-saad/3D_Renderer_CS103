55e557caae8495458a96e4809f56956db41f9930
Sahed Saad <158004018+sahed-saad@users.noreply.github.com>
2025-12-11 18:22:52 -0500

    Refactor Vector2 class
    Added constructors and method definitions for vector operations.

--- Geometry/Vector2.h
+++ Geometry/Vector2.h
@@ -1,13 +1,15 @@
+#ifndef VECTOR2_H
+#define VECTOR2_H
 
-class Vector2{
-    public:
-        float x;
-        float y;
-
-        Vector2(float x, float y);
-
-        Vector2 AddVector(Vector2 vec) const;
-        Vector2 SubtractVector(Vector2 vec) const;
-        Vector2 MultiplyVector(float m) const;
-
+class Vector2 {
+public:
+    float x;
+    float y;
+    Vector2();
+    Vector2(float x, float y);
+    Vector2 AddVector(const Vector2& vec) const;
+    Vector2 SubtractVector(const Vector2& vec) const;
+    Vector2 MultiplyVector(float m) const;
 };
+
+#endif
