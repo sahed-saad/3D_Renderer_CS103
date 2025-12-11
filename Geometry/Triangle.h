f7344b29da810fd521d1d5e2e01e7abe426cb880
Sahed Saad <158004018+sahed-saad@users.noreply.github.com>
2025-12-11 18:23:39 -0500

    refactor
    Added include guards and default constructor to Triangle class.

--- Geometry/Triangle.h
+++ Geometry/Triangle.h
@@ -1,9 +1,13 @@
-class Triangle{
-    public:
-        // indexes to vertexes in Mesh class
-        int point1;
-        int point2;
-        int point3;
+#ifndef TRIANGLE_H
+#define TRIANGLE_H
 
-        Triangle(int i1, int i2, int i3);
+class Triangle {
+public:
+    int point1;
+    int point2;
+    int point3;
+    Triangle();
+    Triangle(int i1, int i2, int i3);
 };
+
+#endif
