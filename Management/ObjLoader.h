c30b67490e040409de39793c1f7428f2f2a1858e
Sahed Saad <158004018+sahed-saad@users.noreply.github.com>
2025-12-12 10:14:39 -0500

    Implement ObjLoader.h

--- Management/ObjLoader.h
+++ Management/ObjLoader.h
@@ -1,8 +1,40 @@
-#include "../Geometry/Mesh.h"
-#include <string>
+#include "../Management/ObjLoader.h"
+#include <fstream>
+OBJ::OBJ(const string& path) {
+    ifstream MyReadFile(path);
+    string line;
+    vector<vector<int>> faces;
 
-class ObjLoader {
-public:
-    // load the 3D object into Mesh
-    bool load(std::string filename, Mesh& outMesh);
-};
+    while (getline(file, line)) {  // read the content from the .obj file that has the vertices and the indices, and go line by line
+        stringstream ss(line);  // Go char by char, which in this case, is vertices by vertices, or edges by edges
+        string type;
+        ss >> type;  // Read the specifi char into type each time it loops through, i.e., vertex and edge
+ 
+        if(type == "v") {
+            float m, n, p;
+
+            ss >> m >> n >> p;
+            vertices.push_back({m, n, p});
+        }
+
+        if(type == "f") {
+            vector<int> face;
+            int index;
+
+            while (ss >> index) {
+                face.push_back(index-1);
+            }
+            faces.push_back(face);
+        }
+
+        for (auto& f:faces) {
+            for (int i = 0; i < f.size(); i++) {
+                int a = f[i];
+                int b = f[(i+1) % f.size()];
+
+                edges.push_back({a,b});
+            }
+        }
+
+    }
+}
